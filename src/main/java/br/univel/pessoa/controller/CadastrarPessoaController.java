da1613eba87aed1065082122fe14a326b860fd5e
Avell 1513 <Avell 1513@192.168.1.46>
2016-11-21 10:54:08 -0200

    Parte 07 - Desenvolvido a parte para realizar cadastro via upload de arquivo XML. Controller de pessoa foi alterado para poder receber e processar o arquivo XML, Adicionado código no arquivo cadastrarPessoa.xhtml para criar uma tab que vai ter as opções para realizar o upload do arquivo XML. Foram utilizados os componentes fileUpload e PrimeFaces para realizar o upload de arquivos xml.

--- src/main/java/br/univel/pessoa/controller/CadastrarPessoaController.java
+++ src/main/java/br/univel/pessoa/controller/CadastrarPessoaController.java
@@ -1,8 +1,20 @@
 package br.univel.pessoa.controller;
 
+import java.io.IOException;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.primefaces.model.UploadedFile;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import br.univel.model.PessoaModel;
 import br.univel.repository.PessoaRepository;
@@ -29,6 +41,25 @@ public class CadastrarPessoaController {
 
 	@Inject //Realiza a injeção de depêndencia dos beans.
 	PessoaRepository pessoaRepository;
+
+	private UploadedFile file;
+
+
+	/**
+	 * Retorna um objeto do tipo file
+	 * @return
+	 */
+	public UploadedFile getFile() {
+		return file;
+	}
+
+	/**
+	 * Passado o valor de "file" por parâmetro para o atributo da classe.
+	 * @param file
+	 */
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
 
 	/**
 	 * Retorna um objeto do tipo pessoaModel
@@ -63,5 +94,79 @@ public class CadastrarPessoaController {
 		Uteis.MensagemInfo("Registro cadastrado com sucesso");
 
 	}
+
+
+	/**
+	 * Realiza o upload de arquivos
+	 */
+	 public void UploadRegistros() {
+
+		 DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+		 try {
+
+
+			 if(this.file.getFileName().equals("")){
+				 Uteis.MensagemAtencao("Nenhum arquivo selecionado!");
+				 return;
+			 }
+
+			 DocumentBuilder builder = factory.newDocumentBuilder();
+
+	                 Document doc = builder.parse(this.file.getInputstream());
+
+	                 Element element = doc.getDocumentElement();
+
+	                 NodeList nodes = element.getChildNodes();
+
+	                for (int i = 0; i < nodes.getLength(); i++) {
+
+	        	     Node node  = nodes.item(i);
+
+	        	    if(node.getNodeType() == Node.ELEMENT_NODE){
+
+	        		 Element elementPessoa =(Element) node;
+
+	        		 //Pegando os valores dos arquivos XML
+	        		 String nome     = elementPessoa.getElementsByTagName("nome").item(0).getChildNodes().item(0).getNodeValue();
+	        		 String sexo     = elementPessoa.getElementsByTagName("sexo").item(0).getChildNodes().item(0).getNodeValue();
+	        		 String email    = elementPessoa.getElementsByTagName("email").item(0).getChildNodes().item(0).getNodeValue();
+	        		 String endereco = elementPessoa.getElementsByTagName("endereco").item(0).getChildNodes().item(0).getNodeValue();
+
+	        		 PessoaModel newPessoaModel = new PessoaModel();
+
+	        		 newPessoaModel.setUsuarioModel(this.usuarioController.GetUsuarioSession());
+	        		 newPessoaModel.setEmail(email);
+	        		 newPessoaModel.setEndereco(endereco);
+	        		 newPessoaModel.setNome(nome);
+	        		 newPessoaModel.setOrigemCadastro("X");
+	        		 newPessoaModel.setSexo(sexo);
+
+	        		 //Salvando um registro que veio do arquivo XML
+	        		 pessoaRepository.SalvarNovoRegistro(newPessoaModel);
+	        	   }
+	              }
+
+
+
+		     Uteis.MensagemInfo("Registros cadastrados com sucesso!");
+
+		} catch (ParserConfigurationException e) {
+
+			e.printStackTrace();
+
+		} catch (SAXException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+
+	 }
+
+
 
 }
